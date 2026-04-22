4cd21bd22b850fd0e9e9616da031c74348c0a726
sitebacinebizea-oss <sitebacinebizea@gmail.com>
2026-04-22 23:25:31 +0300

    Update config.php

--- dashboard/config.php
+++ dashboard/config.php
@@ -1,33 +1,29 @@
 <?php
-
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'mysql.railway.internal');
 
 /** MySQL database username */
-
-define('DB_USER', 'u545956515_andmei');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-
-define('DB_PASSWORD', 'Kmh712326134@');
+define('DB_PASSWORD', 'ljxFqKdlLXvpaeTChkwXBYiGFzwauiVf');
 
 /** MySQL database name */
+define('DB_NAME', 'railway');
 
-define('DB_NAME', 'u545956515_datbseot');
+/** MySQL database port */
+define('DB_PORT', '3306');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
 
 /** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE','');
+define('DB_COLLATE', '');
 
 define('CAN_REGISTER', 'none');
 define('DEFAULT_ROLE', 'member');
 
 // For development only!!
 define('SECURE', false);
-
 define('DEBUG', true);
-
-
 ?>
